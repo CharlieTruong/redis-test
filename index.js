4753d70e26278e993965852190f2f057396cfd41
charlietruong <charlie.m.truong@gmail.com>
2018-02-26 09:14:28 -0600

    Add initial redis connection

---
+++ index.js
@@ -0,0 +1,9 @@
+const redis = require('redis')
+
+const REDIS_PORT = process.env.REDIS_PORT || 6379
+const REDIS_HOST = process.env.REDIS_HOST || '127.0.0.1'
+const client = redis.createClient(REDIS_PORT, REDIS_HOST)
+client.unref()
+client.on('conect', () => {
+  console.log('connected')
+})
